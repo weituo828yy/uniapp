a23bcf08123d2aaeaa7b95f1729be122d71e5365
魏拓 <weituo@cmict.chinamobile.com>
2022-07-27 16:12:26 +0800

    新增uniapp项目框架

---
+++ pages/404/404.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		页面不存在
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
